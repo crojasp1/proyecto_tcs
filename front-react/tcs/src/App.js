7cc4c32078da639614cf1945c02384cdbf7d4255
Jarey22 <juansrm02@gmail.com>
2024-09-05 13:57:45 -0500

    primeras pruebas de front-end

---
+++ front-react/tcs/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Banner/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
