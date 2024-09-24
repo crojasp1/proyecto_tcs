f4b60fd6e97490bb5d2e0cc4df58899e918e092a
Jarey22 <juansrm02@gmail.com>
2024-09-24 00:18:02 -0500

    creacion de que quieres

---
+++ front-react/tcs/src/components/Main3.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import fondo from "../assets/fondo.jpg"
+import "../styles/Main3.css";
+import Button from 'react-bootstrap/Button';
+
+
+
+
+
+function Main3() {
+  return (
+    <div className='Main3'>
+        <div className='contenido'>
+            <img src={fondo} alt="imagen de fondo" id='imagen-fondo'/>
+                <div className='texto'>
+                    <h1>¿Que Quieres?</h1>
+                    <h3>Es una iniciativa para promover y comprar boletas de nuevo eventos siendo tu el creador
+                        o participando en ellos de una manera segura y transparente.
+                    </h3>
+                    <div className='botones'>
+                    <Button id='comprar'>Comprar</Button>
+                    <Button id='vender'>Vender</Button>
+                    </div>
+                </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Main3
