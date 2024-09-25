99733f21154f49f4c8e7ecacecd467a94eebc0cc
ENERIED PRIETO SAENZ <camilorojasprieto@gmail.com>
2024-09-24 22:27:06 -0500

    Creacion de carpeta rutas y controladores para el Evento

---
+++ node-mysql/routes/routes_event.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const eventController = require("../controllers/eventController");
+const bodyParser = require("body-parser");
+
+const router = express.Router();
+router.use(bodyParser);
+
+
+
+
+
+
