9b188aa38fd5535154167df484942489727712c2
Beto2529 <=>
2024-09-26 16:10:52 -0500

    docker volumen

--- node-mysql/app.js
+++ node-mysql/app.js
@@ -1,26 +1,23 @@
-const { createPool } = require('mysql2/promise');
+const { createPool } = require("mysql2/promise");
 const express = require("express");
 const appRoutes = require("./routes/routes");
 const bodyParser = require("body-parser");
 
-
 const pool = createPool({
-  host: 'mysqldb',
-  user: 'root',
+  host: "mysqldb",
+  user: "root",
   password: "112529",
-  port: 3306
-})
-
+  port: 3307,
+});
 
 const PORT = 8087;
 
 const app = express();
 
-
-app.get('/ping', async(req,res)=>{
-  const result = await pool.query('SELECT NOW()')
-  res.json(result[0])
-})
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result[0]);
+});
 
 //routers
 app.use(bodyParser.json());
@@ -31,7 +28,6 @@ app.use("/images", express.static("upload/images"));
 
 app.use(express.urlencoded({ extended: true }));
 
-
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
   console.log(`::::::Aplicación inicializada en el puerto ${PORT}`);
 });
