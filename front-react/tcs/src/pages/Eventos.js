ce8941b3b3c44d83299ea4c362a2059489702ef3
AngyA6365 <camilaaguirre839@gmail.com>
2024-09-25 19:32:57 -0500

    Se realizó formulario en el apartado de Eventos disponibles.

--- front-react/tcs/src/pages/Eventos.js
+++ front-react/tcs/src/pages/Eventos.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "../components/Header"
-import Footer from '../components/Footer'; 
+import Footer from "../components/Footer"
+import Form from "../components/Form"
 
 function Eventos () {
   return (
     <div className='Eventos'>
      <Header/>
+     <Form/>
      <Footer/>
     </div>
   )
